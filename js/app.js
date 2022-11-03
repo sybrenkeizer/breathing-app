f021ceb305d2f625f8881400996a9a2e482fe868
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2022-11-03 10:15:12 +0545

    Finish settings UI

--- js/app.js
+++ js/app.js
@@ -2,13 +2,18 @@
 // TODO - ENHANCE: Add background music
 // TODO - ENHANCE: Add grow animation to circle without messing up z-index hierarchy
 // TODO - ENHANCE: Add responsiveness
-//                 - font
-//                 - elements
+// TODO - ENHANCE: Reduce size menu toggle button
+// TODO - ENHANCE: style select menu arrow
 // TODO - FIX: Fading out trouble with circle button
+// TODO - FIX: Cross browser compatibility 
+// TODO - FIX: Deal with lost text anomaly background
 
 const settingsToggleBtn = document.getElementById('settings-toggle-btn');
 const moveToggleBtn = document.getElementById('move-toggle-btn');
 const settingsContainer = document.getElementById('settings');
+const settingRangeInputs = document.querySelectorAll('input[type=range]');
+const settingNumberInputs = document.querySelectorAll('input[type=number]');
+const settingSelect = document.getElementById('programs-select');
 const pageBackground = document.getElementById('page-background');
 const circleToggleBtn = document.getElementById('circle-btn');
 const indicatorText = document.getElementById('indicator-text');
@@ -24,7 +29,9 @@ const EXHALATION = 3;
 let startNumber;
 let isBreathing = false;
 
-// Assisting functions
+
+
+// ===== Assisting functions ======
 const showBreatherPointer = () => pointerContainer.classList.add('show');
 const hideBreatherPointer = () => pointerContainer.classList.remove('show');
 const showBreatherIndicatorText = () => indicatorText.classList.add('show');
@@ -46,16 +53,25 @@ const changeFadeCircleBtnShort = () => {
 };
 const changeFadeCircleBtnLong = () => {
   if (circleToggleBtn.classList.contains('fade')) {
-    circleToggleBtn.style.transition = `opacity ${COUNTDOWN_VALUE}s linear`;
+    circleToggleBtn.style.transition = `opacity ${2}s linear`;
   };
 };
+const limitNumberLength = (number, maxLength) => {
+  return number.length > maxLength ? number.slice(0, maxLength) : number;
+}
+const limitNumberValue = (number, maxValue) => {
+  return number > maxValue ? number - (number - maxValue) : number;
+}
+const closeSettingsPanel = () => pageBackground.classList.contains('show') && toggleSettings();
 
 
-// Footer function
+
+// ===== Footer function =====
 const setYearFooter = () => footerYearText.textContent = new Date().getFullYear();
 
 
-// Settings functions
+
+// ===== Settings functions ===== 
 const toggleSettings = () => {
   if (settingsContainer.classList.contains('open')) {
     settingsToggleBtn.classList.remove('open');
@@ -70,7 +86,45 @@ const toggleSettings = () => {
   };
 };
 
-// Breathe counting function
+const setFormInputValues = (e) => {
+  let rangeInput;
+  let numberInput;
+  let value;
+
+  if (e.target.type === 'range') {
+    rangeInput = e.target;
+    numberInput = e.target.nextElementSibling;
+    value = rangeInput.value;
+  };
+
+  if (e.target.type === 'number') {
+    rangeInput = e.target.previousElementSibling;
+    numberInput = e.target;
+    value = limitNumberLength(limitNumberValue(numberInput.value, 40), 2);
+  };
+
+  let min = rangeInput.min;
+  let max = rangeInput.max;
+
+  numberInput.value = value;
+  rangeInput.value = numberInput.value === '' ? 0 : value;
+
+  rangeInput.style.backgroundSize = (value - min) * 100 / (max - min) + '% 100%';
+};
+
+const colorFormSelectOption = () => {
+  Array.from(settingSelect.children).forEach(option => {
+    if (!option.disabled && option.selected) {
+      option.style.color = 'hsl(359, 55%, 55%)';
+      settingSelect.style.color = 'hsl(359, 55%, 55%';
+    } else if (!option.disabled) {
+      option.style.color = 'hsl(0, 38%, 54%)';
+    }
+  })
+}
+
+
+// ===== Breathe counting function =====
 const inhalation = () => {
   if (!isBreathing) return;
   let timeInhalation = INHALATION;
@@ -131,7 +185,8 @@ const exhalation = () => {
 };
 
 
-// Breathe interface control functions
+
+// ===== Breathe interface control functions =====
 const startBreathing = () => {
   isBreathing = true;
   showBreatherPauseIcon();
@@ -154,9 +209,7 @@ const startCountdown = () => {
   addFadeBreatherCircleBtn();
   changeFadeCircleBtnLong();
 
-
-  
-  countdownNumber.textContent = COUNTDOWN_VALUE
+  countdownNumber.textContent = COUNTDOWN_VALUE;
 
   const interval = setInterval(() => {
     countdownNumber.textContent = --startNumber;
@@ -182,9 +235,12 @@ const initialize = () => {
   setYearFooter();
 }
 
-// Event Listeners
+// ===== Event Listeners =====
 settingsToggleBtn.addEventListener('click', toggleSettings);
+settingRangeInputs.forEach(range => range.addEventListener('input', setFormInputValues));
+settingNumberInputs.forEach(number => number.addEventListener('input', setFormInputValues));
+settingSelect.addEventListener('change', colorFormSelectOption);
 circleToggleBtn.addEventListener('click', startStopBreather);
 circleToggleBtn.addEventListener('mouseover', changeFadeCircleBtnShort);
 window.addEventListener('DOMContentLoaded', initialize);
-
+pageBackground.addEventListener('click', closeSettingsPanel);
