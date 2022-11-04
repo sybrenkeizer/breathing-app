d404463734f77e8aac6e4201322a473558175bcc
Sybren Keizer <92291687+sybrenkeizer@users.noreply.github.com>
2022-11-04 22:57:22 +0545

    BASIC Settings finished

--- js/app.js
+++ js/app.js
@@ -5,13 +5,17 @@
 // TODO - ENHANCE: Reduce size menu toggle button
 // TODO - ENHANCE: Look into better stop/start icons
 // TODO - ENHANCE: Improve gradient system of indicator circle 
+// TODO - ENHANCE: Add more fluent exit from settings modal
 // TODO - FIX: Fading out trouble with circle button
 // TODO - FIX: Cross browser compatibility 
 // TODO - FIX: Deal with lost text anomaly background
 // TODO - FIX: Timer moves out of sync (after some time)
+// TODO - FIX: Do not allow the same name as custom program
+// TODO - FIX: DO not allow double timing sequences being stored as program
 
 const settingsToggleBtn = document.getElementById('settings-toggle-btn');
 const moveToggleBtn = document.getElementById('move-toggle-btn');
+
 const settingsContainer = document.getElementById('settings');
 const settingSelect = document.getElementById('programs-select');
 const settingRangeInputs = document.querySelectorAll('input[type=range]');
@@ -21,14 +25,24 @@ const exhalationNumberInput = document.getElementById('exhalation-text');
 const inhaledRetentionText = document.getElementById('inhaled-retention-text');
 const exhaledRetentionText = document.getElementById('exhaled-retention-text');
 const settingStartSessionsBtn = document.getElementById('save-session-btn');
-const settingsSaveProgramBtn = document.getElementById('save-program-btn');
-const pageBackground = document.getElementById('page-background');
+const settingsSaveProgramBtn = document.getElementById('store-program-btn');
+
+const settingsModal = document.getElementById('settings-modal');
+const modalSaveBtn = document.getElementById('modal-save-btn');
+const cancelSettingsModal = document.getElementById('cancel-settings-modal');
+const backgroundSettings = document.getElementById('background-settings');
+const backgroundModal = document.getElementById('background-modal');
+const modalCustomProgramName = document.getElementById('name-custom-program-text');
+
 const circleToggleBtn = document.getElementById('circle-btn');
 const indicatorText = document.getElementById('indicator-text');
 const countdownNumber = document.getElementById('countdown-number');
 const pointerContainer = document.getElementById('pointer-container');
 const pointer = document.getElementById('pointer');
+
 const footerYearText = document.getElementById('footer-year');
+
+const MAX_TIME_VALUE = 30;
 
 let setting_countdownValue = 5;
 let setting_inhalationValue = 3;
@@ -39,96 +53,42 @@ let setting_exhaledRetentionValue = 0;
 let startNumber;
 let isBreathing = false;
 let isGradient;
+let isSettingsOpened = false;
+let isSettingsModalOpened = false;
 
-const breathPrograms = [
-  {
-    id: 'sv-b',
-    name: 'Sama vritti',
-    level: 'beginner',
-    breath: {
-      inhalation: 3,
-      exhalation: 3,
-      inhaledRetention: 3,
-      exhaledRetention: 3,
-    }
-  },
+
+
+getCustomProgramFromLS = () => {
+  customBreathPrograms = localStorage.getItem('programs')
+    ? JSON.parse(localStorage.getItem('programs'))
+    : [];
+};
+
+let customBreathPrograms;
+
+const presetBreathPrograms = [
   {
     id: 'sv-i',
     name: 'Sama vritti',
-    level: 'intermediate',
     breath: {
       inhalation: 5,
       exhalation: 5,
       inhaledRetention: 5,
       exhaledRetention: 5,
-    }
-  },
-  {
-    id: 'sv-a',
-    name: 'Sama vritti',
-    level: 'advanced',
-    breath: {
-      inhalation: 10,
-      exhalation: 10,
-      inhaledRetention: 10,
-      exhaledRetention: 10,
-    }
-  },
-  {
-    id: 'sv-e',
-    name: 'Sama vritti',
-    level: 'expert',
-    breath: {
-      inhalation: 20,
-      exhalation: 20,
-      inhaledRetention: 20,
-      exhaledRetention: 20,
-    }
-  },
-  {
-    id: 'av-b',
-    name: 'Anulom vilom',
-    level: 'beginner',
-    breath: {
-      inhalation: 1,
-      exhalation: 4,
-      inhaledRetention: 2,
-      exhaledRetention: 2,
-    }
-  },
-  {
-    id: 'av-i',
-    name: 'Anulom vilom',
-    level: 'intermediate',
-    breath: {
-      inhalation: 2,
-      exhalation: 8,
-      inhaledRetention: 4,
-      exhaledRetention: 4,
-    }
-  },
-  {
-    id: 'av-a',
-    name: 'Anulom vilom',
-    level: 'advanced',
-    breath: {
-      inhalation: 3,
-      exhalation: 9,
-      inhaledRetention: 6,
-      exhaledRetention: 6,
-    }
+    }, 
+    datatype: 'preset'
   },
   {
     id: 'av-e',
     name: 'Anulom vilom',
-    level: 'expert',
     breath: {
       inhalation: 4,
       exhalation: 16,
       inhaledRetention: 8,
       exhaledRetention: 8,
-    }
-  },
+    }, 
+    datatype: 'preset'
+  }
 ]
 
 
@@ -161,10 +121,15 @@ const changeFadeCircleBtnLong = () => {
 const limitNumberLength = (number, maxLength) => {
   return number.length > maxLength ? number.slice(0, maxLength) : number;
 }
-const limitNumberValue = (number, maxValue) => {
-  return number > maxValue ? number - (number - maxValue) : number;
+const limitNumberValue = (number) => {
+  return number > MAX_TIME_VALUE ? number - (number - MAX_TIME_VALUE) : number;
 }
-const closeSettingsPanel = () => pageBackground.classList.contains('show') && toggleSettings();
+const closeSettingsPanel = () => {
+  if (isSettingsOpened && !isSettingsModalOpened) {
+    toggleSettings();
+  };
+};
+
 
 
 
@@ -179,15 +144,18 @@ const setYearFooter = () => footerYearText.textContent = new Date().getFullYear(
 // ==================== 
 const toggleSettings = () => {
   if (settingsContainer.classList.contains('open')) {
+    isSettingsOpened = false;
     settingsToggleBtn.classList.remove('open');
     moveToggleBtn.classList.remove('move');
     settingsContainer.classList.remove('open');
-    pageBackground.classList.remove('show');
+    backgroundSettings.classList.remove('show');
   } else {
+    isSettingsOpened = true;
     settingsToggleBtn.classList.add('open');
     moveToggleBtn.classList.add('move');
     settingsContainer.classList.add('open');
-    pageBackground.classList.add('show');
+    backgroundSettings.classList.add('show');
+    backgroundSettings.addEventListener('click', closeSettingsPanel);
   };
 };
 
@@ -205,7 +173,7 @@ const setFormInputValues = (e) => {
   if (e.target.type === 'number') {
     rangeInput = e.target.previousElementSibling;
     numberInput = e.target;
-    value = limitNumberLength(limitNumberValue(numberInput.value, 40), 2);
+    value = limitNumberLength(limitNumberValue(numberInput.value), 2);
   };
 
   let min = rangeInput.min;
@@ -246,16 +214,15 @@ const initializeSettings = (e) => {
   setRotationTimePointer();  
   setCircleSections();
   closeSettingsPanel();
-}
+};
 
 const validateSettingsFormInput = () => {
-  return (inhalationNumberInput.value && exhalationNumberInput.value) 
-}
+  return (inhalationNumberInput.value > 0 && exhalationNumberInput.value > 0);
+};
 
 const controlInputSettings = () => {
   const setSelectedToCustoms = () => settingSelect.value = 'custom';
 
-  
   settingRangeInputs.forEach(rangeInput => {
     rangeInput.addEventListener('input', setSelectedToCustoms);
   });
@@ -265,16 +232,23 @@ const controlInputSettings = () => {
   });
 
   settingSelect.addEventListener('click', (e) => {
-    if (e.target.value !== 'custom') setBreathInputsToProgram(e);
-  })
-}
+    if (e.target.value !== 'custom') {
+      const targetDatatype = e.target.options[e.target.selectedIndex].dataset.datatype
+      targetDatatype === 'preset'
+        ? setBreathInputsToProgram(e, presetBreathPrograms)
+        : targetDatatype === 'custom'
+        ? setBreathInputsToProgram(e, customBreathPrograms)
+        : console.error('Datatype not found');
+    };
+  });
+};
 
-const setBreathInputsToProgram = (e) => {
+const setBreathInputsToProgram = (e, data) => {
   const selectedProgram = e.target.value;
 
-  for (let key in breathPrograms) {
-    if (selectedProgram === breathPrograms[key].id) {
-      const program = breathPrograms[key];
+  for (let key in data) {
+    if (selectedProgram === data[key].id) {
+      const program = data[key];
       inhalationNumberInput.value = program.breath.inhalation;
       exhalationNumberInput.value = program.breath.exhalation;
       inhaledRetentionText.value = program.breath.inhaledRetention;
@@ -303,15 +277,136 @@ const setBreathInputsToProgram = (e) => {
       setRangeInput(rangeInput, exhaledRetentionText);
     };
 
-  })
+  });
 
+};
+
+const setMaxTimeValueInput = () => {
+  settingRangeInputs.forEach(rangeInput => rangeInput.max = MAX_TIME_VALUE);
+  settingNumberInputs.forEach(numberInput => numberInput.max = MAX_TIME_VALUE);
+};
+
+const openProgramNameModal = (e) => {
+  if (!validateSettingsFormInput()) return;
+  e.preventDefault();
+  isSettingsModalOpened = true;
+  moveToggleBtn.style.opacity = '0';
+  settingsModal.classList.add('show');
+  modalCustomProgramName.select();
+  backgroundModal.classList.add('show');
+  cancelSettingsModal.addEventListener('click', cancelProgramNameModal);
+};
+
+const createCustomProgram = (e) =>{
+  e.preventDefault();
+  saveCustomProgramToLS(e);
+  getCustomProgramFromLS();
+  clearProgramsFromUI();
+  populateUIPresetPrograms();
+  populateUICustomPrograms();
+  closeProgramNameModal();
+  selectNewlyCreatedProgram();
+};
+
+const closeProgramNameModal = () => {
+  modalCustomProgramName.value = '';
+  isSettingsModalOpened = false;
+  settingsModal.classList.remove('show');
+  backgroundModal.classList.remove('show');
+  moveToggleBtn.style.opacity = '1';
+};
+
+const cancelProgramNameModal = () => {
+  closeProgramNameModal();
+};
+
+const populateUIPresetPrograms = () => {
+  let optgroupElPreset;
+
+  if (!!customBreathPrograms.length) {
+    optgroupElPreset = document.createElement('optgroup');
+    optgroupElPreset.label = 'Preset Programs';
+  };
+
+  presetBreathPrograms.forEach(program => {
+    const optionEl = document.createElement('option');
+    optionEl.value = program.id;
+    optionEl.textContent = program.name;
+    optionEl.dataset.datatype = 'preset' 
+
+    !!customBreathPrograms.length
+      ? optgroupElPreset.appendChild(optionEl)
+      : settingSelect.appendChild(optionEl);
+    });
+
+    optgroupElPreset && settingSelect.appendChild(optgroupElPreset);
+};
+
+const populateUICustomPrograms = () => {
+  let optgroupElCustom;
+  if (!!customBreathPrograms.length) {
+    optgroupElCustom = document.createElement('optgroup');
+    optgroupElCustom.label = 'Custom Programs';
+  };
+
+  customBreathPrograms.forEach(program => {
+    const optionEl = document.createElement('option');
+    optionEl.value = program.id;
+    optionEl.textContent = program.name;
+    optionEl.dataset.datatype = 'custom' 
+
+    !!customBreathPrograms.length
+      ? optgroupElCustom.appendChild(optionEl)
+      : settingSelect.appendChild(optionEl);
+    });
+
+    optgroupElCustom && settingSelect.appendChild(optgroupElCustom);
+};
+
+const clearProgramsFromUI = () => {
+  Array.from(settingSelect.children).forEach((child, index) => {
+  index > 1 && child.remove();
+  });
+};
+
+const selectNewlyCreatedProgram = () => {
+  settingSelect.selectedIndex = settingSelect.options.length - 1;
 };
 
 
 
-const saveProgramSettings = (e) => {
+
+// =========================
+// ===== Local Storage =====
+// =========================
+
+const saveCustomProgramToLS = (e) => {
   e.preventDefault();
-}
+  const inhalation = +inhalationNumberInput.value;
+  const exhalation = +exhalationNumberInput.value;
+  const inhaledRetention = inhaledRetentionText.value ? +inhaledRetentionText.value : 0;
+  const exhaledRetention = exhaledRetentionText.value ? +exhaledRetentionText.value : 0;
+  const programName = modalCustomProgramName.value;
+
+  const newProgram = {
+    id: programName,
+    name: programName,
+    breath: {
+      inhalation: inhalation,
+      exhalation: exhalation,
+      inhaledRetention: inhaledRetention,
+      exhaledRetention: exhaledRetention,
+    }, 
+    datatype: 'custom'
+  };
+  const customProgramsLS = localStorage.getItem('programs')
+                          ? JSON.parse(localStorage.getItem('programs'))
+                          : [];
+  customProgramsLS.push(newProgram);
+  localStorage.setItem('programs', JSON.stringify(customProgramsLS));
+};
+
+
 
 
 // ==========================
@@ -319,15 +414,6 @@ const saveProgramSettings = (e) => {
 // ==========================
 const inhalation = () => {
   if (!isBreathing) return;
-
-  console.log(setting_inhalationValue);
-  console.log(setting_exhalationValue);
-  console.log(setting_inhaledRetentionValue);
-  console.log(setting_exhaledRetentionValue);
-
-
-
-
   let timeInhalation = setting_inhalationValue;
   indicatorText.firstElementChild.textContent = timeInhalation;
   indicatorText.lastElementChild.textContent = 'Inhale';
@@ -350,7 +436,6 @@ const inhaledRetention = () => {
   let timeRetention = setting_inhaledRetentionValue;
   indicatorText.firstElementChild.textContent = timeRetention;
   indicatorText.lastElementChild.textContent = 'Hold';
-  
   
   const interval = setInterval(() => {
     timeRetention--;
@@ -390,7 +475,6 @@ const exhaledRetention = () => {
   let timeRetention = setting_exhaledRetentionValue;
   indicatorText.firstElementChild.textContent = timeRetention;
   indicatorText.lastElementChild.textContent = 'Hold';
-  
   
   const interval = setInterval(() => {
     timeRetention--;
@@ -526,6 +610,10 @@ const startStopBreather = () => {
 const initialize = () => {
   setYearFooter();
   controlInputSettings();
+  setMaxTimeValueInput();
+  getCustomProgramFromLS();
+  populateUIPresetPrograms();
+  populateUICustomPrograms();
 }
 
 
@@ -537,8 +625,8 @@ settingRangeInputs.forEach(range => range.addEventListener('input', setFormInput
 settingNumberInputs.forEach(number => number.addEventListener('input', setFormInputValues));
 settingSelect.addEventListener('change', colorFormSelectOption);
 settingStartSessionsBtn.addEventListener('click', initializeSettings);
-settingsSaveProgramBtn.addEventListener('click', saveProgramSettings);
+settingsSaveProgramBtn.addEventListener('click', openProgramNameModal);
+modalSaveBtn.addEventListener('click', createCustomProgram);
 circleToggleBtn.addEventListener('click', startStopBreather);
 circleToggleBtn.addEventListener('mouseover', changeFadeCircleBtnShort);
 window.addEventListener('DOMContentLoaded', initialize);
-pageBackground.addEventListener('click', closeSettingsPanel);
